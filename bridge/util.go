6baf53ca5920c2b2622ff986fcb1fdc1356fe86b
Jon Evans <glitch100@gmail.com>
2019-01-31 14:05:49 +0000

    Registration work

--- bridge/util.go
+++ bridge/util.go
@@ -169,7 +169,11 @@ func serviceSync(b *Bridge, quiet bool, newIP string) {
 	}
 
 	log.Debugf("Syncing services on %d containers", len(containers))
-
+	if newIP != "" {
+		if b.config.HostIp != newIP {
+			log.Info("Bridge Config HostIP is different to new IP, adjusting")
+		}
+	}
 	// NOTE: This assumes reregistering will do the right thing, i.e. nothing..
 	for _, listing := range containers {
 		services := servicesSnapshot[listing.ID]
