39a47bdbe08d195b26f7a7b868fa5ffc7d0157ac
Jamie Snell <jamie.snell@hudl.com>
2019-08-20 11:44:49 -0500

    MARVEL-2642 - Log to console (#55)
    * switch to log to console and use logdriver

--- run-registrator.sh
+++ run-registrator.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-DATE=$(date +%d%m%Y-%H%M%S)
-LOG_FILE=/logs/registrator-$(hostname)-$DATE.log
-exec > >(tee -a ${LOG_FILE} )
-exec 2> >(tee -a ${LOG_FILE} >&2)
 
-# Startup registrator and capture log output
+# Startup registrator, logs will be written to console.
+
 # Needs to run inside the container
 
 CMD="/bin/registrator $@"
